cd54863c5c162192d691b1c7b9bb4355297bc785
harsh9697 <harshchauhan9697@gmail.com>
2016-10-21 10:19:33 +0530

    Add files via upload

---
+++ largest of two numbers by harsh.py
@@ -0,0 +1,9 @@
+n1=int(input('enter a number n1:'))
+n2=int(input('enter a number n2:'))
+if (n1>n2):
+    print(n1,"n1 is the largest number")
+elif (n1==n2):
+    print("the numbers are equal")
+else:
+    print(n2, "n2 is the largest number")
+print('press enter to exit')    
